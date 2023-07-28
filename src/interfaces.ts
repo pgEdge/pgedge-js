eafd99e4d98a0fe68c3ebe9306469d5b92dad755
Curtis Myzie <curtis.myzie@gmail.com>
2023-07-27 22:37:00 -0400

    Attempted latency fix; 0.11

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -39,7 +39,7 @@ interface DatabaseNode {
 
 interface Env {
   PGEDGE_NODES: string;
-  PGEDGE_LATENCY_MEASUREMENT_ENDPOINT?: string;
+  PGEDGE_LATENCY_MEASUREMENT_ENDPOINT?: any;
 }
 
 export { Region, Connection, Geolocation, GeolocationSpec, DatabaseNode, Env };
